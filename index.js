7844c7a008592d032ffc8721a3986dc3e09902b9
Yusuf14218 <sakayusufbahadir1@gmail.com>
2026-03-08 17:16:24 +0300

    asda

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
-const puppeteer = require("puppeteer");
+const mineflayer = require('mineflayer')
+const fs = require('fs');
+const { keep_alive } = require("./keep_alive");
+let rawdata = fs.readFileSync('config.json');
+let data = JSON.parse(rawdata);
+var lasttime = -1;
+var moving = 0;
+var connected = 0;
+var actions = [ 'forward', 'back', 'left', 'right']
+var lastaction;
+var pi = 3.14159;
+var moveinterval = 2; // 2 second movement interval
+var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
+var host = data["ip"];
+var username = data["name"]
+var bot = mineflayer.createBot({
+  host: host,
+  username: username
+});
+function getRandomArbitrary(min, max) {
+       return Math.random() * (max - min) + min;
 
-(async () => {
+}
+bot.on('login',function(){
+	console.log("Logged In")
+});
+bot.on('time', function() {
+    if (connected <1) {
+        return;
+    }
+    if (lasttime<0) {
+        lasttime = bot.time.age;
+    } else {
+        var randomadd = Math.random() * maxrandom * 20;
+        var interval = moveinterval*20 + randomadd;
+        if (bot.time.age - lasttime > interval) {
+            if (moving == 1) {
+                bot.setControlState(lastaction,false);
+                moving = 0;
+                lasttime = bot.time.age;
+            } else {
+                var yaw = Math.random()*pi - (0.5*pi);
+                var pitch = Math.random()*pi - (0.5*pi);
+                bot.look(yaw,pitch,false);
+                lastaction = actions[Math.floor(Math.random() * actions.length)];
+                bot.setControlState(lastaction,true);
+                moving = 1;
+                lasttime = bot.time.age;
+                bot.activateItem();
+            }
+        }
+    }
+});
 
- const browser = await puppeteer.launch({
-  args: ["--no-sandbox"]
- });
+bot.on('spawn',function() {
+    connected=1;
+});
 
- const page = await browser.newPage();
-
- await page.goto("https://aternos.org/go/");
-
- await page.type("#user", " Ybs1234");
- await page.type("#password", "yusuf2009");
-
- await page.click("#login");
-
- await page.waitForTimeout(10000);
-
- await page.goto("https://aternos.org/server/");
-
- await page.waitForTimeout(5000);
-
- await page.click("#start");
-
- console.log("Server başlatıldı");
-
-})();
